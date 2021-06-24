79ecb174f197c00ad7f3ebbfc13e704abf7a2e36
nazishasghar <72311025+nazishasghar@users.noreply.github.com>
2021-06-24 17:58:33 +0530

    Initial Commit

---
+++ Samachar/SamacharApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamacharApp.swift
+//  Samachar
+//
+//  Created by Nazish Asghar on 24/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SamacharApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
